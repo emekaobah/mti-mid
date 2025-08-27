a73f0d9430a44c3cc7904997636f14f5d0e63241
Emeka Obah <emekaobah@gmail.com>
2025-08-27 13:06:13 +0100

    fix page responsiveness

--- app/(public)/form/page.tsx
+++ app/(public)/form/page.tsx
@@ -3,14 +3,14 @@ import React from "react";
 
 const DataCollectionPage = () => {
   return (
-    <main className="min-h-screen  flex flex-col bg-[#F9F7F1] lg:px-15 px-4">
+    <main className="min-h-screen  flex flex-col bg-[#F9F7F1] lg:px-15 px-4 mx-auto">
       <div className="flex-1 flex flex-col justify-center py-8  ">
         <div className="w-full  mx-auto ">
-          <div className="text-center mb-10 w-sm mx-auto ">
+          <div className="text-center mb-10 w-sm mx-auto flex flex-col items-center justify-center">
             <h1 className="text-3xl font-bold mb-2 text-[#074318] ">
-              Nigeria AfCFTA <br /> Trade Intelligence Form
+              Nigeria AfCFTA Trade Intelligence Form
             </h1>
-            <p className="text-muted-foreground max-w-2xl mx-auto">
+            <p className="text-muted-foreground max-w-2xl ">
               Help us understand what you need so Nigerian businesses can
               connect you win.
             </p>
